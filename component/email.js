d62e4913a5e424ed670180a51409f2c1990020ab
OEM Configuration (temporary user) <ginidthakur@mansainfotech.com>
2021-04-04 12:39:42 +0530

    updated email subject

--- component/email.js
+++ component/email.js
@@ -2,8 +2,8 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // true for 465, false for other ports
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USERNAME,
     pass: process.env.EMAIL_PASSWORD,
@@ -11,7 +11,6 @@ const transporter = nodemailer.createTransport({
 });
 
 exports.accountVerificationEmail = (to, token) => {
-  console.log("call email");
   let url = process.env.APP_URL + "/verify/" + token;
   let html =
     "<p>Grazie per esserti registrato con la mail " +
@@ -22,13 +21,11 @@ exports.accountVerificationEmail = (to, token) => {
   let mailOptions = {
     from: process.env.EMAIL_USERNAME,
     to: to,
-    subject: "Verify Your Email",
+    subject: "Verifica il tuo indirizzo Email",
     html: html,
   };
 
   return transporter.sendMail(mailOptions).catch((err) => {
-    console.log("on erro");
-    console.log(err);
     return true;
   });
 };
